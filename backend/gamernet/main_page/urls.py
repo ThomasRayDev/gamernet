d550afafb5b93d0ba825158fba22e77cd2b4dda4
DimaGDD <dimagelenidze21@gmail.com>
2024-09-07 15:32:48 +0300

    Add sign_up
    Пока что только тестовый вариант

--- backend/gamernet/main_page/urls.py
+++ backend/gamernet/main_page/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("main_page/<int:question_id>/", views.detail, name="detail"),
-    path("main_page/<int:question_id>/results/", views.results, name="results"),
-    path("main_page/<int:question_id>/vote/", views.vote, name="vote"),
-    path("test/", views.test, name="test"),
+    path("sign_up/", views.sign_up, name="register"),
 ]
